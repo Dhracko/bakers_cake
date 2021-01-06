11e9fbdc3d4f009e03d10f52d547f9fd75e09ce6
Frankie <dracko@hotmail.co.uk>
2021-01-06 18:45:00 +0000

    Created add to basket form

--- basket/contexts.py
+++ basket/contexts.py
@@ -7,7 +7,9 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    # free_delivery_collection = settings.FREE_DELIVERY_COLLECTION
 
+    # Calculate the cost of delivery and it's free if it past the threshold
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
@@ -24,6 +26,7 @@ def basket_contents(request):
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        # 'free_delivery_collection': settings.FREE_DELIVERY_COLLECTION,
         'grand_total': grand_total,
     }
 
