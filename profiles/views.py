37e5e5951b191ce9eadc76cf9e7c1f051c5161f8
Frankie <dracko@hotmail.co.uk>
2021-01-17 13:55:38 +0000

    Added profile updated templates, messages and views

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,redirect, reverse, get_object_or_404, HttpResponse
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -9,9 +11,26 @@ def profile(request):
      """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.INFO, 'Profile updated \
+                                                        successfully')
+        else:
+            messages.error(request, 'Sorry, there was a problem \
+                            updating the profile right now.\
+                            Please try again later.')
+            return redirect(reverse('profile'))
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
---
+++ static/readme_docs/test.md/test.md
@@ -0,0 +1,5 @@
+For testing card payments please use the following:
+* US: 4242 4242 4242 4242 04/24 242 42424(ZIP)
+* UK: 4000 0082 6000 0000 04/24 242 (same postcode as form)
+
+for further information about payment cards please refer [here](https://stripe.com/docs/testing#international-cards)
