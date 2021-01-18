cb7a11322bdcb958d392938861f509930afb5a2e
Frankie <dracko@hotmail.co.uk>
2021-01-18 09:44:28 +0000

    Created forms.py in Products

---
+++ products/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    """
+    Will extend the ModelForm for admin to manage products and categories
+    """
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-blue rounded'
