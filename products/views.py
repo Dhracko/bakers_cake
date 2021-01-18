be4272b53933016e9a9528326feb152dcaacba7e
Frankie <dracko@hotmail.co.uk>
2021-01-18 17:29:54 +0000

    Edit & Delete product in place

--- products/views.py
+++ products/views.py
@@ -76,9 +76,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added cake!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add cake. Please ensure the "oven is on" and the form is valid.')
     else:
@@ -115,3 +115,12 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a cake in the website """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Cake deleted!')
+
+    return redirect(reverse('products'))
