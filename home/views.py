7707bbdabff48a53591f6d15f7154bed11d20910
Frankie <dracko@hotmail.co.uk>
2021-01-20 16:45:35 +0000

    Created contact app and moved settings

--- home/views.py
+++ home/views.py
@@ -1,41 +1,6 @@
-from django.shortcuts import render, redirect
-from django.core.mail import EmailMessage
-from .forms import ContactForm
-from django.template.loader import get_template
+from django.shortcuts import render
 
 
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
-
-
-def contact(request):
-    form_class = ContactForm
-
-    # new logic!
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            contact_name = request.POST.get('contact_name', '')
-            contact_email = request.POST.get('contact_email', '')
-            form_content = request.POST.get('content', '')
-
-            # Email the profile with the
-            # contact information
-            template = get_template('contact_template.txt')
-            context = {
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content,
-            }
-            content = template.render(context)
-
-            email = EmailMessage("New contact form submission", content,
-                                 "Your website" + '', ['youremail@gmail.com'],
-                                 headers={'Reply-To': contact_email}
-                                 )
-            email.send()
-            return redirect('contact')
-
-    return render(request, 'contact.html', {'form': form_class, })
